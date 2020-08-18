523313a8850fe81178f3af7d6b1a15f1d65aaa23
Rahul Sonparote <P10479367@capita.co.uk>
2020-08-18 18:11:04 +0530

    Base Solution added

---
+++ Promotion/Promotion.Models/Unit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Promotion.Models
+{
+    public class Unit
+    {
+        public string UnitName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Promotion/Promotion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayActivePromotion();
+            var item = DisplayUnitMenu();
+            var Quantity = DisplayQuantity();
+            Console.WriteLine("You have selected {0} and quantity is {1}",item,Quantity);
+        }
+        static public int DisplayQuantity()
+        {
+            Console.WriteLine("Enter Quantity");
+            Console.WriteLine();
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+        static public int DisplayUnitMenu()
+        {
+            Console.WriteLine("Select Unit");
+            Console.WriteLine();
+            Console.WriteLine("1. Unit A");
+            Console.WriteLine("2. Unit B");
+            Console.WriteLine("3. Unit C");
+            Console.WriteLine("4. Unit D");
+            
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+        static public void DisplayActivePromotion()
+        {
+            Console.WriteLine("*******Active Promotions********");
+            Console.WriteLine();
+            Console.WriteLine("1. 3 Of Unit A's for 130");
+            Console.WriteLine("2. 2 Of Unit B's for 45");
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
